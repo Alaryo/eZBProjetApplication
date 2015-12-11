fee9b96a8583ff92fecec5a719064c4833529868
Hicham <hicham.daher@eleves.ec-nantes.fr>
2015-12-11 14:18:44 +0100

    MAJ de la méthode Demande String

--- eZoomBook/src/ezoombook/Utilitaire.java
+++ eZoomBook/src/ezoombook/Utilitaire.java
@@ -5,15 +5,18 @@
  */
 package ezoombook;
 
+import java.io.IOException;
 import java.util.InputMismatchException;
 import java.util.Scanner;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  *
  * @author pierre
  */
 public class Utilitaire {
-    
+
     public static int DemandeInt() {
         int i;
         Scanner saisieUtilisateur = new Scanner(System.in);
@@ -25,30 +28,34 @@ public class Utilitaire {
             System.out.println("entrez un nombre!");
             i = DemandeInt();
         }
-        
-        if (i<0){
-        System.out.println("entrez un nombre positif");
-            i = DemandeInt();
-    }
-        return i;
-    }
-    
-    public static String DemandeString(){
-        String txt;
-        Scanner saisieUtilisateur = new Scanner(System.in);
 
-        try {
-            txt = saisieUtilisateur.next();
-
-        } catch (InputMismatchException e) {
-            System.out.println("entrez un nombre!");
+        if (i < 0) {
+            System.out.println("entrez un nombre positif");
             i = DemandeInt();
         }
-        
-        if (i<0){
-        System.out.println("entrez un nombre positif");
-            i = DemandeInt();
-    }
         return i;
-    } 
+    }
+
+    public static String DemandeString() {
+        String txt = "";
+        boolean saisieOk = false;
+        Scanner saisieUtilisateur = new Scanner(System.in);
+
+        while (saisieOk = false) {
+            txt = saisieUtilisateur.next();
+            System.out.println(txt);
+            try {
+                System.in.read();
+            } catch (IOException ex) {
+                Logger.getLogger(Utilitaire.class.getName()).log(Level.SEVERE, null, ex);
+            }
+            System.out.println("Est-ce que ce texte vous convient ? Si oui tapez 1.");
+            if (saisieUtilisateur.next().equals("1")){
+                saisieOk = true;
+            } else {
+                System.out.println("Entrez votre nouveau texte");
+            }
+        }
+        return txt;
+    }
 }
